752eb1a15189040816b893aef79bb760c5ae0cfe
LOGESH <logeshvicky103@gmail.com>
2024-11-29 19:43:21 +0530

    Update prob_2.py

--- Basics problems/problems set_1/prob_2.py
+++ Basics problems/problems set_1/prob_2.py
@@ -1,14 +1,12 @@
-'''Prob 2: 
-
-Write a program to check whether a triangle can be formed with the given values for the angles.
+'''Write a program to check whether a triangle can be formed with the given values for the angles.
 If sum of angles is equal to 180, then triangle can be formed, else it can't be formed.
 Input: 45 45 45
 Expected Output:
 Triangle cannot be formed
 Explanation -> We are getting 3 inputs, that is three angles of triangle, but here the sum of three angles 
-that is 45+45+45 is not equal to 180 so Triangle cannot be formed is the output
+that is 45+45+45 is not equal to 180 so Triangle cannot be formed is the output'''
 
-answer:'''
+solution:
 
 #input from user
 a=int(input("Enter the number:"))
