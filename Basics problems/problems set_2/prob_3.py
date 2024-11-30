08efc0f30cbdfa18a0eef7a70b4556da181755fb
LOGESH R <logeshvicky103@gmail.com>
2024-11-30 19:19:56 +0530

    Update prob_3.py

--- Basics problems/problems set_2/prob_3.py
+++ Basics problems/problems set_2/prob_3.py
@@ -1,1 +1,19 @@
+'''Write a program to get firstName and lastName and n as input and print fullName that is firstName+lastName for n times.
+Input
+Nandy
+Raja
+5
+Expected output:
+NandyRaja
+NandyRaja
+NandyRaja
+NandyRaja
+NandyRaja
+Explanation - Nandy is the firstName, Raja is the lastName and n is 5, so the expected output has NandyRaja printed 5 times'''
+solution:
 
+firstName="Logesh"
+lastName="Vicky"
+n=int(input("Enter:"))
+for i in range(n):
+    print(firstName+lastName)
