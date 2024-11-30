d2a224a6566fa1adcfafe5a44a192405cd0630d9
LOGESH R <logeshvicky103@gmail.com>
2024-11-30 19:22:23 +0530

    Update prob_3.py

--- Basics problems/problems set_2/prob_3.py
+++ Basics problems/problems set_2/prob_3.py
@@ -10,10 +10,11 @@ NandyRaja
 NandyRaja
 NandyRaja
 Explanation - Nandy is the firstName, Raja is the lastName and n is 5, so the expected output has NandyRaja printed 5 times'''
+
 solution:
 
-firstName="Logesh"
-lastName="Vicky"
+firstName=input("Enter:")
+lastName=input("Enter:")
 n=int(input("Enter:"))
 for i in range(n):
     print(firstName+lastName)
