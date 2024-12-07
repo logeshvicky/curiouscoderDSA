fa7e9995c65be235c0812ff18a90a56d294a17c3
LOGESH R <logeshvicky103@gmail.com>
2024-12-07 17:46:36 +0530

    Create prob_9.py

---
+++ Basics problems/problems set_3/prob_9.py
@@ -0,0 +1,22 @@
+pattern 9:
+
+n=4
+for i in range(1,n+1):
+  for j in range(1,i+1):
+    print（"*",end=" ")
+  print()
+for a in range(n-1,0, -1):
+  for a in range(a):
+    print("*",end=" ")
+  print()
+
+output:
+*
+* *
+* * *
+* * * *
+* * *
+* *
+*
+
+
