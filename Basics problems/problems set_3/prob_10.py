3d63efc2f6f0769039afb4489ebffd124988b02c
LOGESH R <logeshvicky103@gmail.com>
2024-12-07 22:00:26 +0530

    Update prob_10.py

--- Basics problems/problems set_3/prob_10.py
+++ Basics problems/problems set_3/prob_10.py
@@ -1,1 +1,18 @@
+pattern 10:
 
+n = 4
+for i in range(1, n+1):
+    for j in range(n, i, -1):
+        print(" ", end="")
+    for j in range(1, i+1):
+        print("*", end="")
+    for j in range(1, i):
+        print("*", end="")
+    print()
+
+output:
+
+   *
+  ***
+ *****
+*******
